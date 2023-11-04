2476aeebd9b5f08a4456f4144a8516256b4a8ba0
akhdanrgya <akhdan.anargya@gmail.com>
2023-11-05 00:14:55 +0700

    update post and home to fetch post

--- src/components/ui/forms/PostForm.tsx
+++ src/components/ui/forms/PostForm.tsx
@@ -1,11 +1,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -14,27 +14,48 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "../textarea";
 import FileUploader from "../shared/FileUploader";
+import { PostValidation } from "@/lib/validation";
+import { Models } from "appwrite";
+import { useUserContext } from "@/context/AuthContext";
+import { useToast } from "../use-toast";
+import { useCreatePost } from "@/lib/react-query/queriesAndMutations";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+type PostFormProps = {
+  post?: Models.Document;
+};
 
-const PostForm = () => {
+const PostForm = ({ post }: PostFormProps) => {
+  const {mutateAsync: createPost, isPending: isLoading} = useCreatePost()
+  const { user } = useUserContext()
+  const { toast } = useToast()
+  const navigate = useNavigate()
+
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof PostValidation>>({
+    resolver: zodResolver(PostValidation),
     defaultValues: {
-      username: "",
+      caption: post ? post?.caption : "",
+      file: [],
+      location: post ? post?.location : "",
+      tags: post ? post?.tags.join(",") : "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof PostValidation>) {
+    const newPost = await createPost({
+      ...values,
+      userId: user.id,
+    })
+
+    if(!newPost){
+      toast({
+        title: 'Please try again'
+      })
+    }
+
+    navigate('/')
+
   }
   return (
     <Form {...form}>
@@ -65,7 +86,10 @@ const PostForm = () => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Photos</FormLabel>
               <FormControl>
-                <FileUploader />
+                <FileUploader
+                  fieldChange={field.onChange}
+                  mediaUrl={post?.imageUrl}
+                />
               </FormControl>
               <FormMessage className="shad-form_message" />
             </FormItem>
@@ -78,7 +102,7 @@ const PostForm = () => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Location</FormLabel>
               <FormControl>
-                <Input type="text" className="shad-input" />
+                <Input type="text" className="shad-input" {...field} />
               </FormControl>
               <FormMessage className="shad-form_message" />
             </FormItem>
@@ -97,6 +121,7 @@ const PostForm = () => {
                   type="text"
                   className="shad-input"
                   placeholder="Art, Sport, School"
+                  {...field}
                 />
               </FormControl>
               <FormMessage className="shad-form_message" />
@@ -107,7 +132,12 @@ const PostForm = () => {
           <Button type="button" className="shad-button_dark_4">
             Cancel
           </Button>
-          <Button type="submit" className="shad-button_primary whitespace-nowrap">Submit</Button>
+          <Button
+            type="submit"
+            className="shad-button_primary whitespace-nowrap"
+          >
+            Submit
+          </Button>
         </div>
       </form>
     </Form>
--- src/components/ui/shared/FileUploader.tsx
+++ src/components/ui/shared/FileUploader.tsx
@@ -1,8 +1,63 @@
+import React, { useCallback } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
+import { useState } from "react";
+import { Button } from "../button";
 
-const FileUploader = () => {
+type FileUploaderProps = {
+  fieldChange: (FILES: file[]) => void;
+  mediaUrl: string;
+};
+
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+  const [file, setFile] = useState<File[]>([]);
+  const [fileUrl, setFileUrl] = useState("");
+
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+    },
+    [file]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".png", ".jpg", ".jpeg", ".svg"],
+    },
+  });
   return (
-    <div>FileUploader</div>
-  )
-}
+    <div
+      {...getRootProps()}
+      className="flex flex-center flex-col bg-dark-3 rounded-x1 cursor-pointer"
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+      {fileUrl ? (
+        <>
+        <div className="flex flex-1 justify-center w-full p-5 lg:p-10">
+          <img src={fileUrl} alt="image" className="file_uploader-img"/>
+        </div>
+          <p className="file_uploader-label">Click or drag photo to replace</p>
+        </>
+      ) : (
+        <div className="file_uploader-box">
+          <img
+            src="/assets/icons/file-upload.svg"
+            alt="file upload"
+            width={96}
+            height={77}
+          />
+          <h3 className="base-medium text-light-2 mb-2 mt-6">
+            Drag photo here
+          </h3>
+          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
 
-export default FileUploader
+          <Button className="shad-button_dark_4">Select from device</Button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploader;
